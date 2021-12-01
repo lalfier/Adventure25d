d10425827f6763436cadf8a40dd09ed9eb94856c
lalfier <lalfier@gmail.com>
2021-12-01 15:57:32 +0100

    Added - Parallax effect on background hills image.

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField]
+    float parallaxSpeed;    //Paralax movement speed
+
+    float length;
+    float startPos;
+
+    void Awake()
+    {
+        //Get default values
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float temp = (Camera.main.transform.position.x * (1 - parallaxSpeed));
+        float dist = (Camera.main.transform.position.x * parallaxSpeed);
+
+        //Set X position
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        //Loop left or right
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
